d5110af8a6dc3640fac80d82d4e99d237ba14b55
Maribel-PC\Maribel <maribel.chaverra@udea.edu.co>
2017-08-27 18:28:05 -0500

    4. Multiple Components  - Proyecto "Tour of Heroes"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule 
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
